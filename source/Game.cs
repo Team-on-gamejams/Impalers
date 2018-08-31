1e91fbe41172bad88697723c43cb1d56d886909c
Team-on <timonsol13@gmail.com>
2018-08-31 15:30:03 +0300

    Done left bot impale

--- source/Game.cs
+++ source/Game.cs
@@ -24,23 +24,23 @@ namespace Impalers {
 		public ushort scorePlayer, scoreEnemy;
 		public ushort counterMeatPlayer, counterMeatEnemy, counterImpalePlayer, counterImpaleEnemy;
 
-		public Grid TopGrid		{ get; set; }
-		public Grid BottomGrid	{ get; set; }
-		public Grid LeftGrid	{ get; set; }
-		public Grid RightGrid	{ get; set; }
+		public Grid TopGrid { get; set; }
+		public Grid BottomGrid { get; set; }
+		public Grid LeftGrid { get; set; }
+		public Grid RightGrid { get; set; }
 
-		List<Tuple<byte, byte, byte>> avaliableImpales = new List<Tuple<byte, byte, byte>>(10);
+		List<Tuple<byte, byte, byte, byte>> avaliableImpales = new List<Tuple<byte, byte, byte, byte>>(10);
 
 		public Game() {
-			for (byte i = 0; i < Settings.sizeY; ++i)
-				for (byte j = 0; j < Settings.sizeX; ++j)
+			for(byte i = 0; i < Settings.sizeY; ++i)
+				for(byte j = 0; j < Settings.sizeX; ++j)
 					map[i, j] = new GameCell();
 		}
 
 		public void Start(Enemy enemy) {
 			this.enemy = enemy;
-			for (byte i = 0; i < Settings.sizeY; ++i)
-				for (byte j = 0; j < Settings.sizeX; ++j)
+			for(byte i = 0; i < Settings.sizeY; ++i)
+				for(byte j = 0; j < Settings.sizeX; ++j)
 					map[i, j].Init();
 			isPlayerTurn = true;
 			scorePlayer = scoreEnemy = 0;
@@ -51,16 +51,16 @@ namespace Impalers {
 
 			byte randomImageNumber;
 			do {
-				randomImageNumber = (byte)Singletons.rand.Next(1, 11);
-			} while (randomImageNumber == (y + 1 == Settings.sizeY ? 255: map[y + 1, x].imageNumber) ||
+				randomImageNumber = (byte) Singletons.rand.Next(1, 11);
+			} while(randomImageNumber == (y + 1 == Settings.sizeY ? 255 : map[y + 1, x].imageNumber) ||
 				randomImageNumber == (y - 1 == -1 ? 255 : map[y - 1, x].imageNumber) ||
 				randomImageNumber == (x + 1 == Settings.sizeX ? 255 : map[y, x + 1].imageNumber) ||
-				randomImageNumber == (x - 1 == -1 ? 255 : map[y, x - 1].imageNumber) || 
+				randomImageNumber == (x - 1 == -1 ? 255 : map[y, x - 1].imageNumber) ||
 
-				randomImageNumber == (x - 1 == -1				|| y - 1 == -1				? 255 : map[y - 1, x - 1].imageNumber) ||
-				randomImageNumber == (x + 1 == Settings.sizeX	|| y - 1 == -1				? 255 : map[y - 1, x + 1].imageNumber) ||
-				randomImageNumber == (x - 1 == -1				|| y + 1 == Settings.sizeY	? 255 : map[y + 1, x - 1].imageNumber) ||
-				randomImageNumber == (x + 1 == Settings.sizeX	|| y + 1 == Settings.sizeY	? 255 : map[y + 1, x + 1].imageNumber) 
+				randomImageNumber == (x - 1 == -1 || y - 1 == -1 ? 255 : map[y - 1, x - 1].imageNumber) ||
+				randomImageNumber == (x + 1 == Settings.sizeX || y - 1 == -1 ? 255 : map[y - 1, x + 1].imageNumber) ||
+				randomImageNumber == (x - 1 == -1 || y + 1 == Settings.sizeY ? 255 : map[y + 1, x - 1].imageNumber) ||
+				randomImageNumber == (x + 1 == Settings.sizeX || y + 1 == Settings.sizeY ? 255 : map[y + 1, x + 1].imageNumber)
 			);
 
 			map[y, x].imageNumber = randomImageNumber;
@@ -88,7 +88,7 @@ namespace Impalers {
 			//};
 			//t.Start();
 
-			if (isPlayerTurn) {
+			if(isPlayerTurn) {
 				++scorePlayer;
 				++counterMeatPlayer;
 			}
@@ -100,34 +100,35 @@ namespace Impalers {
 		}
 
 		public void PlaceStick(byte startX, byte startY, byte endX, byte endY) {
-			short dx = (short)(startX - endX), dy = (short)(startY - endY);
+			short dx = (short) (startX - endX), dy = (short) (startY - endY);
 			if(dx == 0 || dy == 0) {
-
 				for(byte i = Math.Min(startX, endX); i <= Math.Max(startX, endX); ++i)
-					if (!map[startY, i].isMeat || map[startY, i].isStickStart || map[startY, i].isStickBody || (i != endX && map[startY, i].isStickEnd))
+					if(!map[startY, i].isMeat || map[startY, i].isStickStart || map[startY, i].isStickBody || (i != endX && map[startY, i].isStickEnd))
 						return;
-				for (byte i = Math.Min(startY, endY); i <= Math.Max(startY, endY); ++i)
-					if (!map[i, startX].isMeat || map[i, startX].isStickStart || map[i, startX].isStickBody || (i != endY && map[i, startX].isStickEnd))
+				for(byte i = Math.Min(startY, endY); i <= Math.Max(startY, endY); ++i)
+					if(!map[i, startX].isMeat || map[i, startX].isStickStart || map[i, startX].isStickBody || (i != endY && map[i, startX].isStickEnd))
 						return;
 
 				//MessageBox.Show($"{dx} {dy}");
-				
-				ushort lengthImpale = (ushort)(Math.Max(Math.Abs(dy), Math.Abs(dx)) + 1);
-				if (lengthImpale >= 3) {
-					if (isPlayerTurn) {
-						scorePlayer += (ushort)((lengthImpale - 2) * 2);
+
+				ushort lengthImpale = (ushort) (Math.Max(Math.Abs(dy), Math.Abs(dx)) + 1);
+				if(lengthImpale >= 3) {
+
+
+					if(isPlayerTurn) {
+						scorePlayer += (ushort) ((lengthImpale - 2) * 2);
 						++counterImpalePlayer;
 					}
 					else {
-						scoreEnemy += (ushort)((lengthImpale - 2) * 2);
+						scoreEnemy += (ushort) ((lengthImpale - 2) * 2);
 						++counterImpaleEnemy;
 					}
 
 					string styleNum = (Singletons.rand.Next(0, 2) == 1 ? "1" : "2");
 
 					//Down impale
-					if (dy < 0 && (startY == 0 || map[startY - 1, startX].IsEmpty())) {
-						if (startY != 0) {
+					if(dy < 0 && (startY == 0 || map[startY - 1, startX].IsEmpty())) {
+						if(startY != 0) {
 							map[startY - 1, startX].imageStick.Source = CreateBitmapImage("Resources/Pl" + (isPlayerTurn ? '1' : '2') +
 								'/' + styleNum + "Start.png", Rotation.Rotate90);
 							map[startY - 1, startX].isStickStart = true;
@@ -138,7 +139,7 @@ namespace Impalers {
 								'/' + styleNum + "Start.png", Rotation.Rotate90);
 						}
 
-						while (dy++ != 0) {
+						while(dy++ != 0) {
 							map[startY - dy, startX].imageStick.Source = CreateBitmapImage("Resources/Pl" + (isPlayerTurn ? '1' : '2') +
 								'/' + styleNum + "Body.png", Rotation.Rotate90);
 							map[startY - dy, startX].isStickBody = true;
@@ -146,9 +147,9 @@ namespace Impalers {
 						}
 
 						Image img = map[endY, endX].imageStick;
-						if(img.Source != null) 
-							for(byte i = 0; i < 3; ++i) 
-								if(map[endY, endX].imageStickEnd[i].Source == null) { 
+						if(img.Source != null)
+							for(byte i = 0; i < 3; ++i)
+								if(map[endY, endX].imageStickEnd[i].Source == null) {
 									img = map[endY, endX].imageStickEnd[i];
 									break;
 								}
@@ -159,11 +160,11 @@ namespace Impalers {
 					}
 
 					//Up impale
-					else if (dy > 0 && (startY == Settings.sizeY - 1 || map[startY + 1, startX].IsEmpty())) {
-						if (startY != Settings.sizeY - 1) {
-							map[startY + 1, startX].imageStick.Source = CreateBitmapImage("Resources/Pl" + (isPlayerTurn ? '1' : '2')  +
+					else if(dy > 0 && (startY == Settings.sizeY - 1 || map[startY + 1, startX].IsEmpty())) {
+						if(startY != Settings.sizeY - 1) {
+							map[startY + 1, startX].imageStick.Source = CreateBitmapImage("Resources/Pl" + (isPlayerTurn ? '1' : '2') +
 								'/' + styleNum + "Start.png", Rotation.Rotate90);
-							map[startY + 1, startX].imageStick.RenderTransform = new ScaleTransform(1,-1);
+							map[startY + 1, startX].imageStick.RenderTransform = new ScaleTransform(1, -1);
 							map[startY + 1, startX].isStickStart = true;
 							map[startY + 1, startX].imageStick.VerticalAlignment = VerticalAlignment.Stretch;
 						}
@@ -172,18 +173,18 @@ namespace Impalers {
 								'/' + styleNum + "Start.png", Rotation.Rotate90);
 						}
 
-						while (dy-- != 0) {
+						while(dy-- != 0) {
 							map[startY - dy, startX].imageStick.Source = CreateBitmapImage("Resources/Pl" + (isPlayerTurn ? '1' : '2') +
 								'/' + styleNum + "Body.png", Rotation.Rotate90);
 							map[startY - dy, startX].isStickBody = true;
 							map[startY - dy, startX].imageStick.VerticalAlignment = VerticalAlignment.Stretch;
-							map[startY - dy, startX].imageStick.RenderTransform = new ScaleTransform(1,-1);
+							map[startY - dy, startX].imageStick.RenderTransform = new ScaleTransform(1, -1);
 						}
 
 						Image img = map[endY, endX].imageStick;
-						if (img.Source != null) 
-							for (byte i = 0; i < 3; ++i)
-								if (map[endY, endX].imageStickEnd[i].Source == null) { 
+						if(img.Source != null)
+							for(byte i = 0; i < 3; ++i)
+								if(map[endY, endX].imageStickEnd[i].Source == null) {
 									img = map[endY, endX].imageStickEnd[i];
 									break;
 								}
@@ -191,12 +192,12 @@ namespace Impalers {
 						img.Source = CreateBitmapImage("Resources/Pl" + (isPlayerTurn ? '1' : '2') +
 									'/' + styleNum + "End.png", Rotation.Rotate90);
 						img.VerticalAlignment = VerticalAlignment.Stretch;
-						img.RenderTransform = new ScaleTransform(1,-1);
+						img.RenderTransform = new ScaleTransform(1, -1);
 					}
 
 					//Left impale
-					else if (dx > 0 && (startX == Settings.sizeX - 1 || map[startY, startX + 1].IsEmpty())) {
-						if (startX != Settings.sizeX - 1) {
+					else if(dx > 0 && (startX == Settings.sizeX - 1 || map[startY, startX + 1].IsEmpty())) {
+						if(startX != Settings.sizeX - 1) {
 							map[startY, startX + 1].imageStick.Source = CreateBitmapImage("Resources/Pl" + (isPlayerTurn ? '1' : '2') +
 								'/' + styleNum + "Start.png", Rotation.Rotate0);
 							map[startY, startX + 1].imageStick.RenderTransform = new ScaleTransform(-1, 1);
@@ -208,7 +209,7 @@ namespace Impalers {
 								'/' + styleNum + "Start.png", Rotation.Rotate0);
 						}
 
-						while (dx-- != 0) {
+						while(dx-- != 0) {
 							map[startY, startX - dx].imageStick.Source = CreateBitmapImage("Resources/Pl" + (isPlayerTurn ? '1' : '2') +
 								'/' + styleNum + "Body.png", Rotation.Rotate0);
 							map[startY, startX - dx].isStickBody = true;
@@ -217,9 +218,9 @@ namespace Impalers {
 						}
 
 						Image img = map[endY, endX].imageStick;
-						if (img.Source != null)
-							for (byte i = 0; i < 3; ++i)
-								if (map[endY, endX].imageStickEnd[i].Source == null) {
+						if(img.Source != null)
+							for(byte i = 0; i < 3; ++i)
+								if(map[endY, endX].imageStickEnd[i].Source == null) {
 									img = map[endY, endX].imageStickEnd[i];
 									break;
 								}
@@ -231,8 +232,8 @@ namespace Impalers {
 					}
 
 					//Right impale
-					else if (dx < 0 && (startX == 0 || map[startY, startX - 1].IsEmpty())) {
-						if (startX != 0) {
+					else if(dx < 0 && (startX == 0 || map[startY, startX - 1].IsEmpty())) {
+						if(startX != 0) {
 							map[startY, startX - 1].imageStick.Source = CreateBitmapImage("Resources/Pl" + (isPlayerTurn ? '1' : '2') +
 								'/' + styleNum + "Start.png", Rotation.Rotate0);
 							map[startY, startX - 1].isStickStart = true;
@@ -243,7 +244,7 @@ namespace Impalers {
 								'/' + styleNum + "Start.png", Rotation.Rotate0);
 						}
 
-						while (dx++ != 0) {
+						while(dx++ != 0) {
 							map[startY, startX - dx].imageStick.Source = CreateBitmapImage("Resources/Pl" + (isPlayerTurn ? '1' : '2') +
 								'/' + styleNum + "Body.png", Rotation.Rotate0);
 							map[startY, startX - dx].isStickBody = true;
@@ -251,9 +252,9 @@ namespace Impalers {
 						}
 
 						Image img = map[endY, endX].imageStick;
-						if (img.Source != null)
-							for (byte i = 0; i < 3; ++i)
-								if (map[endY, endX].imageStickEnd[i].Source == null) {
+						if(img.Source != null)
+							for(byte i = 0; i < 3; ++i)
+								if(map[endY, endX].imageStickEnd[i].Source == null) {
 									img = map[endY, endX].imageStickEnd[i];
 									break;
 								}
@@ -292,47 +293,64 @@ namespace Impalers {
 							if(x + 1 == Settings.sizeX || map[y, x + 1].IsEmpty()) {
 								lastImpLength = CheckImpaleLeft(x, y);
 								if(lastImpLength != 0)
-									avaliableImpales.Add(new Tuple<byte, byte, byte>(x, y, 0));
+									avaliableImpales.Add(new Tuple<byte, byte, byte, byte>(x, y, 0, lastImpLength));
 							}
 
 							if(y + 1 == Settings.sizeY || map[y + 1, x].IsEmpty()) {
 								lastImpLength = CheckImpaleUp(x, y);
 								if(lastImpLength != 0)
-									avaliableImpales.Add(new Tuple<byte, byte, byte>(x, y, 1));
+									avaliableImpales.Add(new Tuple<byte, byte, byte, byte>(x, y, 1, lastImpLength));
 							}
 
 							if(x == 0 || map[y, x - 1].IsEmpty()) {
 								lastImpLength = CheckImpaleRight(x, y);
 								if(lastImpLength != 0)
-									avaliableImpales.Add(new Tuple<byte, byte, byte>(x, y, 2));
+									avaliableImpales.Add(new Tuple<byte, byte, byte, byte>(x, y, 2, lastImpLength));
 							}
 
 							if(y == 0 || map[y - 1, x].IsEmpty()) {
 								lastImpLength = CheckImpaleDown(x, y);
 								if(lastImpLength != 0)
-									avaliableImpales.Add(new Tuple<byte, byte, byte>(x, y, 3));
+									avaliableImpales.Add(new Tuple<byte, byte, byte, byte>(x, y, 3, lastImpLength));
 							}
 						}
 					}
 				}
 
 				if(avaliableImpales.Count != 0) {
-					avaliableImpales.Sort((a, b) => b.Item3 - a.Item3);
+					avaliableImpales.Sort((a, b) => b.Item4 - a.Item4);
 					var i = avaliableImpales[0];
-					if(i.Item3 == 0)
-						PlaceStick(i.Item1, i.Item2, (byte)(i.Item1 - i.Item3), i.Item2);
+
+					if(i.Item3 == 0) {
+						MessageBox.Show(string.Format("{0} {1} {2} {3}", i.Item1, i.Item2, (byte) (i.Item1 - i.Item4), i.Item2));
+						PlaceStick(i.Item1, i.Item2, (byte) (i.Item1 - i.Item4), i.Item2);
+					}
 					else if(i.Item3 == 2)
-						PlaceStick(i.Item1, i.Item2, (byte) (i.Item1 + i.Item3), i.Item2);
+						PlaceStick(i.Item1, i.Item2, (byte) (i.Item1 + i.Item4), i.Item2);
 					else if(i.Item3 == 1)
-						PlaceStick(i.Item1, i.Item2, i.Item1, (byte)(i.Item2 - i.Item3));
+						PlaceStick(i.Item1, i.Item2, i.Item1, (byte) (i.Item2 - i.Item4));
 					else if(i.Item3 == 3)
-						PlaceStick(i.Item1, i.Item2, i.Item1, (byte) (i.Item2 + i.Item3));
+						PlaceStick(i.Item1, i.Item2, i.Item1, (byte) (i.Item2 + i.Item4));
 				}
 
 				return isPlayerTurn;
 
 				byte CheckImpaleLeft(byte x, byte y) {
-					return 0;
+					int X = x;
+
+					byte len = 0;
+					while(--X != -1) {
+						--x;
+						if(!map[y, x].isMeat || map[y, x].isStickBody || map[y, x].isStickStart)
+							break;
+						++len;
+						if(map[y, x].isStickEnd)
+							break;
+					}
+
+					if(len < 2)
+						len = 0;
+					return len;
 				}
 
 				byte CheckImpaleRight(byte x, byte y) {
@@ -410,9 +428,9 @@ namespace Impalers {
 
 			bool PlaceOnAnyPos() {
 				List<Tuple<byte, byte>> pos = new List<Tuple<byte, byte>>();
-				for(byte x = 0; x < Settings.sizeX; ++x) 
-					for(byte y = 0; y < Settings.sizeY; ++y) 
-						if(map[y, x].IsEmpty()) 
+				for(byte x = 0; x < Settings.sizeX; ++x)
+					for(byte y = 0; y < Settings.sizeY; ++y)
+						if(map[y, x].IsEmpty())
 							pos.Add(new Tuple<byte, byte>(x, y));
 
 				if(pos.Count == 0)
@@ -426,12 +444,12 @@ namespace Impalers {
 		}
 
 		public GameOverResult IsGameOver() {
-			for (byte i = 0; i < Settings.sizeY; ++i)
-				for (byte j = 0; j < Settings.sizeX; ++j)
-					if (map[i, j].IsEmpty())
+			for(byte i = 0; i < Settings.sizeY; ++i)
+				for(byte j = 0; j < Settings.sizeX; ++j)
+					if(map[i, j].IsEmpty())
 						return GameOverResult.None;
-			return	scoreEnemy == scorePlayer ? GameOverResult.Draw :
-					(scoreEnemy < scorePlayer ? GameOverResult.WinPlayer : 
+			return scoreEnemy == scorePlayer ? GameOverResult.Draw :
+					(scoreEnemy < scorePlayer ? GameOverResult.WinPlayer :
 					GameOverResult.WinEnemy);
 		}
 	}
